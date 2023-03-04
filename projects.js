aa3478b1cd8da0e1c656a2bfbaa9dbe5e8252a53
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-03-03 21:29:48 -0300

    Change p projects.index styles

--- projects.js
+++ projects.js
@@ -5,10 +5,10 @@ const projects = [
     link: 'https://pixels-art-iota.vercel.app/',
     img: 'assets/img/pixels-art.gif',
   },
-  trybeWarts = {
-    title: 'TrybeWarts',
-    description: 'A proposta deste projeto era montar um formulário para a escola TrybeWarts utilizando HTML e CSS. Também foi utilizado JavaScript para o contador de caracteres do textArea.',
-    link: '',
-    img: '',
-  }
+  // trybeWarts = {
+  //   title: 'TrybeWarts',
+  //   description: 'A proposta deste projeto era montar um formulário para a escola TrybeWarts utilizando HTML e CSS. Também foi utilizado JavaScript para o contador de caracteres do textArea.',
+  //   link: '',
+  //   img: '',
+  // }
 ];
