aa9bd2a70f77985258a6adbb7091d980532359fa
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-07-13 20:04:46 -0300

    Create info.js

---
+++ src/info.js
@@ -0,0 +1,20 @@
+export const info = {
+  intro: {
+    title: 'Olá!',
+    content: ['Meu nome é Bianca e este é meu portfolio.'],
+    imgSrc: 'https://avatars.githubusercontent.com/u/108292121?v=4',
+    imgAlt: 'Sou uma mulher de pele morena e cabelos escuros abaixo do ombro. Estou usando uma camiseta preta',
+    className: 'intro',
+  },
+  aboutMe: {
+    title: 'Sobre mim',
+    content: ['Paulistana, aos 17 anos me mudei para Botucatu/SP atrás do sonho de ser Médica Veterinária e lá me formei e fiz residência em anestesiologia pela Unesp.', 'De volta a São Paulo, concluí pós-graduação em terapia intensiva e, após 7 anos de atuação na área, tomei a decisão de realizar uma transição de carreira.', 'Busco na área de tecnologia a oportunidade de combinar minha paixão por soluções de problemas com a criação de algo que impacte positivamente na vida das pessoas de uma forma universal e acessível.'],
+    className: 'about-me'
+  },
+}
+
+export const github_URL = 'https://github.com/biancashiromoto';
+
+export const linkedin_URL = 'https://www.linkedin.com/in/bshiromoto/';
+
+export const repos_URL = 'https://api.github.com/users/biancashiromoto/repos'
