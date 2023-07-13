1bd34c2bf8db64029a18e94ae1364162e05b76ff
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-07-13 20:23:37 -0300

    Add children prop to Section component

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,16 +1,17 @@
+import './Section.css';
 
 function Section({ props, children }) {
   const { title, content, imgSrc, imgAlt, className } = props;
-  console.log(children);
+  
   return (
     <section className={ `${className}-container` }>
-      <h2>{ title }</h2>
-      {content && content.map((p, index) => 
-      <p key={ index }>{ p }</p>)}
       <img
         src={ imgSrc }
         alt={ imgAlt }
       />
+      <h2>{ title }</h2>
+      {content && content.map((p, index) => 
+      <p key={ index }>{ p }</p>)}
       { children }
     </section>
   )
