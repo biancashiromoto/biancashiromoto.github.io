35b59e44589d5072e9a01510c0b2d47bb3d8ea7f
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-07-15 20:47:48 -0300

    Remove unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { info } from './info';
 import Home from './pages/Home/Home';
 
 function App() {
