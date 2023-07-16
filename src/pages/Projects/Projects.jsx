1bcbbf54ae90a4969b6b44ddad078772fb400163
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-07-15 21:07:05 -0300

    Implement fetchProjects function in Projects page

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,6 +1,18 @@
-
+import { useEffect, useState } from "react";
+import { fetchAPI } from "../../helpers/fetchAPI";
+import { repos_URL } from "../../info";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = async () =>{
+      const newProjects = await fetchAPI(repos_URL);
+      setProjects(newProjects);
+    }
+    fetchProjects();
+  }, []);
+
   return (
     <div>
       <h2>Projects</h2>
