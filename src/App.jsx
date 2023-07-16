af956cff13912288a5939aeb8913d76f72d348f8
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-07-15 21:06:24 -0300

    Add Projects route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
+import Projects from './pages/Projects/Projects';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/projects" element={ <Projects /> } />
         <Route exact path="/" element={ <Home /> } />
       </Routes>
     </Router>
