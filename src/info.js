cf0291d2df591b55cf7c9d8ea3044c10cc71f6f6
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-07-16 17:40:32 -0300

    Add english translation

--- src/info.js
+++ src/info.js
@@ -1,4 +1,4 @@
-export const info = {
+export const info_pt = {
   intro: {
     title: 'Olá!',
     content: ['Meu nome é Bianca e este é meu portfolio.'],
@@ -8,7 +8,22 @@ export const info = {
   },
   aboutMe: {
     title: 'Sobre mim',
-    content: ['Paulistana, aos 17 anos me mudei para Botucatu/SP atrás do sonho de ser Médica Veterinária e lá me formei e fiz residência em anestesiologia pela Unesp.', 'De volta a São Paulo, concluí pós-graduação em terapia intensiva e, após 7 anos de atuação na área, tomei a decisão de realizar uma transição de carreira.', 'Busco na área de tecnologia a oportunidade de combinar minha paixão por soluções de problemas com a criação de algo que impacte positivamente na vida das pessoas de uma forma universal e acessível.'],
+    content: ['Paulistana, aos 17 anos me mudei para Botucatu/SP atrás do sonho de ser Médica Veterinária e lá me formei e fiz residência em anestesiologia pela Unesp.', 'De volta a São Paulo, concluí pós-graduação em terapia intensiva e, após sete anos de atuação na área, tomei a decisão de realizar uma transição de carreira.', 'Busco na área de tecnologia a oportunidade de combinar minha paixão por soluções de problemas com a criação de algo que impacte positivamente na vida das pessoas de uma forma universal e acessível.'],
+    className: 'about-me'
+  },
+}
+
+export const info_en = {
+  intro: {
+    title: 'Hello!',
+    content: ['My name is Bianca and this is my portfolio.'],
+    imgSrc: 'https://avatars.githubusercontent.com/u/108292121?v=4',
+    imgAlt: 'I am a woman with dark skin and dark hair below the shoulders. I am wearing a black t-shirt',
+    className: 'intro',
+  },
+  aboutMe: {
+    title: 'About me',
+    content: ['I am originally from São Paulo, Brazil and at the age of 17 I moved to Botucatu, SP in pursuit of my dream of becoming a Veterinarian, where I also specialized in anesthesiology at Unesp.', 'After returning to São Paulo, I completed a post-graduate program in intensive care and, after seven years of professional experience, I made the decision to transition my career.', 'I am now seeking opportunities in the technology industry to combine my passion for problem-solving with creating something that positively impacts people\'s lives universally and accessibly.'],
     className: 'about-me'
   },
 }
