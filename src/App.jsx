0a6f0172f8b1358e7c12776a4fd41e0fbbdcd4cc
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-07-16 21:38:18 -0300

    Add Route to Certificates page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
 import Projects from './pages/Projects/Projects';
+import Certificates from './pages/Certificates/Certificates';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/projects" element={ <Projects /> } />
+        <Route path="/certificates" element={ <Certificates /> } />
         <Route exact path="/" element={ <Home /> } />
       </Routes>
     </Router>
