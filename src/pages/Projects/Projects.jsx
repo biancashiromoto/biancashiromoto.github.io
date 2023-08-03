65812e7488a19eb75f01f0a03f80ec20a1ff68c2
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-03 19:49:48 -0300

    Update Projects component

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,42 +1,34 @@
-import { useEffect, useState } from "react";
-import { fetchAPI } from "../../helpers/fetchAPI";
-import { repos_URL } from "../../helpers/info";
+import { useState, useEffect } from 'react';
+import { repos_URL } from '../../helpers/info';
+import { fetchAPI } from '../../helpers/fetchAPI';
+import Card from '../../components/Card/Card';
+import { Link } from 'react-router-dom';
 import './Projects.css';
-import ProjectCard from "../../components/ProjectCard/ProjectCard";
-import ReturnButton from "../../components/ReturnButton/ReturnButton";
 
-function Projects() {
+const Projects = () => {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     const fetchProjects = async () => {
-      const fetchedProjects = await fetchData(repos_URL);
-      const filteredProjects = fetchedProjects
-        .filter((project) => !project.name.includes('biancashiromoto'));
+      const response = await fetchAPI(repos_URL);
+      const filteredProjects = response.filter((project) => !project.name.includes('biancashiromoto'));
       const sortedProjects = filteredProjects.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
       setProjects(sortedProjects);
     }
     fetchProjects();
   }, []);
 
-  const fetchData = async (URL) =>{
-    const data = await fetchAPI(URL);
-    return data;
-  }
-
   return (
-    <div className="projects-page">
-      <ReturnButton />
-      <div className="projects--container">
-        <h2>Projects</h2>
-        {projects && projects.map((project) => (
-            <ProjectCard
-              props={ project }
-              key={ project.id }
-            />
-        ))}
-      </div>
-    </div>
+    <article className='projects-container text-center p-3 vh-100'>
+      <Link
+        className='home-link'
+        to='/'
+      />
+      <h2>Projects</h2>
+      {projects.map((project) => (
+        <Card key={project.id} props={ project } />
+      ))}
+    </article>
   )
 }
 
