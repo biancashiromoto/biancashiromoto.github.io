a72aa3aff94c275d48bb6896670f6d800850e2f9
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-04 17:10:05 -0300

    Add Projects page info

--- src/helpers/info.js
+++ src/helpers/info.js
@@ -11,6 +11,9 @@ export const info_pt = {
     content: ['Paulistana, aos 17 anos me mudei para Botucatu/SP atrás do sonho de ser Médica Veterinária e lá me formei e fiz residência em anestesiologia pela Unesp.', 'De volta a São Paulo, concluí pós-graduação em terapia intensiva e, após sete anos de atuação na área, tomei a decisão de realizar uma transição de carreira.', 'Busco na área de tecnologia a oportunidade de combinar minha paixão por soluções de problemas com a criação de algo que impacte positivamente na vida das pessoas de uma forma universal e acessível.'],
     className: 'about-me'
   },
+  projects: {
+    content: ['Aqui estão alguns dos meus projetos que têm interfaces visuais, mas no meu GitHub você pode conferir outros projetos que não têm!']
+  }
 }
 
 export const info_en = {
@@ -26,6 +29,9 @@ export const info_en = {
     content: ['I am originally from São Paulo, Brazil and at the age of 17 I moved to Botucatu, SP in pursuit of my dream of becoming a Veterinarian, where I also specialized in anesthesiology at Unesp.', 'After returning to São Paulo, I completed a post-graduate program in intensive care and, after seven years of professional experience, I made the decision to transition my career.', 'I am now seeking opportunities in the technology industry to combine my passion for problem-solving with creating something that positively impacts people\'s lives universally and accessibly.'],
     className: 'about-me'
   },
+  projects: {
+    content: ['Here are some of my projects that have visual interfaces, but you can check my GitHub for other projects that don\'t!']
+  }
 }
 
 export const github_URL = 'https://github.com/biancashiromoto';
