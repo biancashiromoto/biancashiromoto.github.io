ec8caecd3292fbd594856d57f69efeefed8a27ce
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-04 19:29:25 -0300

    Remove console.log from formatTitle

--- src/helpers/formatTitle.js
+++ src/helpers/formatTitle.js
@@ -1,7 +1,5 @@
 export const formatTitle = (title) => {
   const spacedTitle = title.replace('-', ' ');
-  // return spacedTitle.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1).join(' '));
-  console.log(spacedTitle.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' '));
   const newTitle = spacedTitle.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
   return newTitle;
 };
