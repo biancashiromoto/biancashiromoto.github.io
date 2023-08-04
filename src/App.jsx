84be6cc6225ac2972fd83ac88ace54e1669fa1cc
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-04 17:30:51 -0300

    Wrap App component in Provider

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home/Home';
 import Projects from './pages/Projects/Projects';
 import Certificates from './pages/Certificates/Certificates';
+import Provider from './context/Provider';
 
 function App() {
   return (
     <Router>
+      <Provider>
       <Routes>
         <Route path="/projects" element={ <Projects /> } />
         <Route path="/certificates" element={ <Certificates /> } />
         <Route exact path="/" element={ <Home /> } />
       </Routes>
+      </Provider>
     </Router>
   )
 }
