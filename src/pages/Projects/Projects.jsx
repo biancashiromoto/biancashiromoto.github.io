8e616c9ef17278efedb3762dbd2c6817fe5d06d5
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-04 16:20:31 -0300

    Implement ReturnButton component in Projects page

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,9 +1,13 @@
 import './Projects.css';
 import ProjectsCarousel from '../../components/ProjectsCarousel/ProjectsCarousel';
+import ReturnButton from '../../components/ReturnButton/ReturnButton';
 
 const Projects = () => {
   return (
-    <ProjectsCarousel />
+    <div className='projects-container text-center p-3 mx-auto vh-sm-100'>
+      <ReturnButton />
+      <ProjectsCarousel />
+    </div>
   )
 }
 
