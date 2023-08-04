c154ef43de00a157d7c6c0f94c5de60ffa91d6df
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-04 18:45:31 -0300

    Set observation text in portuguese

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -7,18 +7,20 @@ import { context } from '../../context/context';
 
 const Projects = () => {
   const { isInEnglish } = useContext(context);
+  const [content, obs] = info_pt.projects.content;
   return (
     <div className='projects-container text-center p-3 mx-auto vh-100'>
       <ReturnButton />
       <h2>{isInEnglish ? 'Projects' : 'Projetos'}</h2>
       {isInEnglish ? (
         info_en.projects.content.map((paragraph, index) => (
-          <p key={ index }>{paragraph}</p>
+          <p className='fs-6' key={ index }>{paragraph}</p>
         ))
       ) : (
-        info_pt.projects.content.map((paragraph, index) => (
-          <p key={ index }>{paragraph}</p>
-        ))
+        <>
+          <p className='fs-6'>{content}</p>
+          <span className='observation small'>{obs}</span>
+        </>
       )}
       <ProjectsCarousel />
     </div>
