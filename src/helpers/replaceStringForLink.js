16dd9248773883c39ef64cb4e7b9a17d6472a9c7
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-04 18:23:19 -0300

    Delete function replaceStringForLink

--- src/helpers/replaceStringForLink.js
+++
@@ -1,8 +0,0 @@
-export const replaceStringForLink = (text, string, link) => {
-  if (text.includes(string)) {
-    text.replace(string, 
-      <a href={link} target='_blank' rel="noreferrer">{string}</a>
-    );
-  }
-  return text;
-};
