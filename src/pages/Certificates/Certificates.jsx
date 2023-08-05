31c52009c28c9d97ac01cae376774d7de160756c
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-05 05:09:51 -0300

    Implement Loading component in Certificates page

--- src/pages/Certificates/Certificates.jsx
+++ src/pages/Certificates/Certificates.jsx
@@ -5,9 +5,10 @@ import './Certificates.css';
 import ReturnButton from '../../components/ReturnButton/ReturnButton';
 import { context } from '../../context/context';
 import LanguageToggle from '../../components/LanguageToggle/LanguageToggle';
+import Loading from '../../components/Loading/Loading';
 
 const Certificates = () => {
-  const { isInEnglish } = useContext(context);
+  const { isInEnglish, isLoading, setIsLoading } = useContext(context);
   const [certificates, setCertificates] = useState([]);
 
   useEffect(() => {
@@ -19,42 +20,48 @@ const Certificates = () => {
     fetchCertificates();
   }, []);
 
+  useEffect(() => setIsLoading(false), []);
+
   return (
     <div className='certificates-container text-center p-3 h-100 mx-auto'>
-      <ReturnButton />
-      <LanguageToggle />
-      <h2>{isInEnglish ? 'Certificates' : 'Certificados'}</h2>
-      {isInEnglish ? (
-        info_en.certificates.content.map((paragraph, index) => (
-          <p className='fs-6' key={index}>{paragraph}</p>
-        ))
-      ) : (
-        info_pt.certificates.content.map((paragraph, index) => (
-          <p className='fs-6' key={index}>{paragraph}</p>
-        ))
-      )}
-      <section className='d-sm-flex flex-column d-md-grid'>
-        <div className='certificates-container row g-md-1 justify-content-center' style={{ gap: '1rem' }}>
-          {certificates.map((certificate) => (
-            <div
-              key={ certificate.id }
-              className={ `certificate-card flex-column align-items-center justify-content-center col-md-4 col-lg-3 d-flex bg-light`}
-            >
-              <a
-                href={certificate.url}
-                target='_blank'
-                rel="noreferrer"
-                className='certificate-title text-decoration-none'
-              >
-                {certificate.title}
-              </a>
-              <p className={ `certificate-school small text-muted d-flex align-items-center ${certificate.school}` }>
-                {certificate.school}
-              </p>
+      {isLoading ? <Loading /> : (
+        <>
+          <ReturnButton />
+          <LanguageToggle />
+          <h2>{isInEnglish ? 'Certificates' : 'Certificados'}</h2>
+          {isInEnglish ? (
+            info_en.certificates.content.map((paragraph, index) => (
+              <p className='fs-6' key={index}>{paragraph}</p>
+            ))
+          ) : (
+            info_pt.certificates.content.map((paragraph, index) => (
+              <p className='fs-6' key={index}>{paragraph}</p>
+            ))
+          )}
+          <section className='d-sm-flex flex-column d-md-grid'>
+            <div className='certificates-container row g-md-1 justify-content-center' style={{ gap: '1rem' }}>
+              {certificates.map((certificate) => (
+                <div
+                  key={ certificate.id }
+                  className={ `certificate-card flex-column align-items-center justify-content-center col-md-4 col-lg-3 d-flex bg-light`}
+                >
+                  <a
+                    href={certificate.url}
+                    target='_blank'
+                    rel="noreferrer"
+                    className='certificate-title text-decoration-none'
+                  >
+                    {certificate.title}
+                  </a>
+                  <p className={ `certificate-school small text-muted d-flex align-items-center ${certificate.school}` }>
+                    {certificate.school}
+                  </p>
+                </div>
+              ))}
             </div>
-        ))}
-        </div>
-      </section>
+          </section>
+        </>
+      )}
     </div>
   )
 }
