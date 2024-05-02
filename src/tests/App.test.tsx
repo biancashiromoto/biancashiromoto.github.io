62c8dda741b8c9e6b4d0fa522466366462d13b12
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-02 19:51:23 -0300

    test: Validate toggle-language-button

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 import Information from "../helpers/classes/Information";
-import React from "react";
 
 describe("Home page", () => {
   beforeEach(() => {
@@ -17,5 +16,10 @@ describe("Home page", () => {
   test("Should contain a title with the text 'Bianca'", () => {
     const title = screen.getByRole("heading");
     expect(title.innerHTML).toContain("Bianca");
+  });
+
+  test("Should contain a button with data-testid 'toggle-language-button'", () => {
+    const toggleLanguageButton = screen.getByTestId("toggle-language-button");
+    expect(toggleLanguageButton).toBeInTheDocument();
   });
 });
