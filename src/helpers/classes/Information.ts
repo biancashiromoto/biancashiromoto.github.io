d44c00f6f2ef37591c78218a52125e707745d367
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-02 20:02:57 -0300

    refactor: Implement constructor on creation of class Information to take language as a parameter

--- src/helpers/classes/Information.ts
+++ src/helpers/classes/Information.ts
@@ -1,11 +1,20 @@
 export default class Information {
   public _profilePictureURL: string = "https://avatars.githubusercontent.com/u/108292121?v=4";
-  public _profilePictureAltText: string = "I am a woman with dark brown eyes and hair and I am wearing a black and gray T-shirt.";
-  public _greetingMessage: string[] = [
-    "Hello",
-    "My name is ",
-    "Bianca",
-    " and this is my portfolio!"
-  ];
+  public _profilePictureAltText: string;
+  public _greetingMessage: string[];
 
+  constructor(language: string) {
+    this._profilePictureAltText = language === "en" ? "I am a woman with dark brown eyes and hair and I am wearing a black and gray T-shirt." : "Sou uma mulher de cabelos e olhos cabelos escuros e estou usando uma camiseta cinza e preta.";
+    this._greetingMessage = language === "en" ? [
+      "Hello",
+      "My name is ",
+      "Bianca",
+      " and this is my portfolio!"
+    ] : [
+      "Olá",
+      "Meu nome é ",
+      "Bianca",
+      " e esse é meu portfolio!"
+    ];
+  }
 }
