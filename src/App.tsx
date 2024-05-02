76a023d357fd99f648e10467e85bf8a2fdb32930
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-02 19:44:57 -0300

    feat: Implement greeting message

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import React from "react";
 import "./App.css";
+import Information from "./helpers/classes/Information";
 
 function App() {
-
+  const information = new Information();
+  
   return (
     <>
       <main>
+        {information._greetingMessage.map((paragraph, index) => {
+          if (paragraph === "Bianca") {
+            return (<h1 key={index}>{paragraph}</h1>);
+          }
+          return (
+            <p key={index}>{paragraph}</p>
+          );
+        })}
         <img
-          src="https://avatars.githubusercontent.com/u/108292121?v=4"
-          alt="I am a woman with dark brown eyes and hair and I am wearing a black and gray T-shirt."
+          src={information._profilePictureURL}
+          alt={information._profilePictureAltText}
         />
       </main>
     </>
