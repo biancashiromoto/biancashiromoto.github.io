268877c7baa0889857f21443fbfd896637b46750
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-02 19:42:16 -0300

    chore: Update React import

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 import Information from "../helpers/classes/Information";
+import React from "react";
 
 describe("Home page", () => {
   beforeEach(() => {
