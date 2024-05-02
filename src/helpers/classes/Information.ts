0d23282adb561938b686194bd629fcce47cbb4c9
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-02 19:21:27 -0300

    feat: Create class Information and attributes profilPictureURL and profilePictureAltText

---
+++ src/helpers/classes/Information.ts
@@ -0,0 +1,4 @@
+export default class Information {
+  public _profilePictureURL: string = "https://avatars.githubusercontent.com/u/108292121?v=4";
+  public _profilePictureAltText: string = "I am a woman with dark brown eyes and hair and I am wearing a black and gray T-shirt.";
+}
