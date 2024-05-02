4be223de69efe2099562220922b76d22e3ac9d51
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-02 19:23:04 -0300

    feat: Add profile picture

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ function App() {
 
   return (
     <>
-      <h1>Portfolio</h1>
+      <main>
+        <img
+          src="https://avatars.githubusercontent.com/u/108292121?v=4"
+          alt="I am a woman with dark brown eyes and hair and I am wearing a black and gray T-shirt."
+        />
+      </main>
     </>
   )
 }
