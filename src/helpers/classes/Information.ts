ed800c11bf493d22f490081ecefc4102fd93137b
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-02 19:44:39 -0300

    chore: Add attribute greetingMessage to class Information

--- src/helpers/classes/Information.ts
+++ src/helpers/classes/Information.ts
@@ -1,4 +1,11 @@
 export default class Information {
   public _profilePictureURL: string = "https://avatars.githubusercontent.com/u/108292121?v=4";
   public _profilePictureAltText: string = "I am a woman with dark brown eyes and hair and I am wearing a black and gray T-shirt.";
+  public _greetingMessage: string[] = [
+    "Hello",
+    "My name is ",
+    "Bianca",
+    " and this is my portfolio!"
+  ];
+
 }
