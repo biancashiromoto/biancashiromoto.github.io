06ba94c481c8bcd3b62ce63c4006f2ba4c6d73f6
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-02 16:24:07 -0300

    chore: Update description about me

--- src/helpers/info.js
+++ src/helpers/info.js
@@ -11,7 +11,7 @@ export const info_pt = {
   },
   aboutMe: {
     title: 'Sobre mim',
-    content: ['Paulistana, aos 17 anos me mudei para Botucatu/SP atrás do sonho de ser Médica Veterinária e lá me formei e fiz residência em anestesiologia pela Unesp.', 'De volta a São Paulo, concluí pós-graduação em terapia intensiva e, após sete anos de atuação na área, tomei a decisão de realizar uma transição de carreira.', 'Busco na área de tecnologia a oportunidade de combinar minha paixão por soluções de problemas com a criação de algo que impacte positivamente na vida das pessoas de uma forma universal e acessível.'],
+    content: ['Paulistana, aos 17 anos me mudei para Botucatu/SP atrás do sonho de ser Médica Veterinária e lá me formei e fiz residência em anestesiologia pela Unesp.', 'De volta a São Paulo, concluí pós-graduação em terapia intensiva e, após sete anos de atuação na área, tomei a decisão de realizar uma transição de carreira.', 'Hoje sou desenvolvedora full-stack e procuro combinar minha paixão por soluções de problemas com a criação de algo que impacte positivamente na vida das pessoas de uma forma universal e acessível.'],
     className: 'about-me'
   },
   projects: {
@@ -41,7 +41,7 @@ export const info_en = {
   },
   aboutMe: {
     title: 'About me',
-    content: ['I am originally from São Paulo, Brazil and at the age of 17 I moved to Botucatu, SP in pursuit of my dream of becoming a Veterinarian, where I also specialized in anesthesiology at Unesp.', 'After returning to São Paulo, I completed a post-graduate program in intensive care and, after seven years of professional experience, I made the decision to transition my career.', 'I am now seeking opportunities in the technology industry to combine my passion for problem-solving with creating something that positively impacts people\'s lives universally and accessibly.'],
+    content: ['I am originally from São Paulo, Brazil and at the age of 17 I moved to Botucatu, SP in pursuit of my dream of becoming a Veterinarian, where I also specialized in anesthesiology at Unesp.', 'After returning to São Paulo, I completed a post-graduate program in intensive care and, after seven years of professional experience, I made the decision to transition my career.', 'Today, I am a full-stack developer seeking to combine my passion for problem-solving with creating something that positively impacts people\'s lives in an universal and accessible way.'],
     className: 'about-me'
   },
   projects: {
