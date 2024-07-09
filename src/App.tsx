eb6ae5e44ea6bde9b497af8bc2f6630cf3673ba5
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-07-09 15:44:06 -0300

    refactor: Create rendering functions to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import "./index.css";
 import Information from "./helpers/classes/Information";
 import Utils from "./helpers/classes/Utils";
@@ -15,51 +15,73 @@ function App() {
 
   const [isLanguagePortuguese, setIsLanguagePortuguese] = useState<boolean>(utils.isLanguagePortuguese());
   
-  return (
-    <>
-      <header>
-        <Button.Root
-          onClick={() => setIsLanguagePortuguese(prevState => !prevState)}
-          testId={dataTestIds.buttons.toggleModeButton}
-        >
-          {isLanguagePortuguese ? ptInformation._translateButtonLabel : enInformation._translateButtonLabel}
-        </Button.Root>
-      </header>
-      <main>
-        {ptInformation._greetingMessage.map((paragraph, index) => {
-          if (paragraph === ptInformation._name) {
-            return (<h1 key={index}>{paragraph}</h1>);
-          }
-          return (
-            <p key={index}>{isLanguagePortuguese ? paragraph : enInformation._greetingMessage[index]}</p>
-          );
-        })}
-        <img
-          src={ptInformation._profilePictureURL}
-          alt={isLanguagePortuguese ? ptInformation._profilePictureAltText : enInformation._profilePictureAltText}
-        />
-        <div>
-          <Link.Root
-            ariaLabel={ariaLabel.links.github}
-            href={ptInformation._githubLink}
-            testid={dataTestIds.links.github}
-          >
-            <FaGithub />
-          </Link.Root>
-          <Link.Root
-            ariaLabel={ariaLabel.links.linkedin}
-            href={ptInformation._linkedinLink}
-            testid={dataTestIds.links.linkedin}
-          >
-            <FaLinkedin />
-          </Link.Root>
-        </div>
-      </main>
+  const renderHeader = (): ReactNode => {
+    return (<header>
+      <Button.Root
+        onClick={() => setIsLanguagePortuguese(prevState => !prevState)}
+        testId={dataTestIds.buttons.toggleModeButton}
+      >
+        {isLanguagePortuguese ? ptInformation._translateButtonLabel : enInformation._translateButtonLabel}
+      </Button.Root>
+    </header>);
+  }
+
+  const renderGreetingMessage = (): ReactNode => {
+    return ptInformation._greetingMessage.map((paragraph, index) => {
+      if (paragraph === ptInformation._name) {
+        return (<h1 key={index}>{paragraph}</h1>);
+      }
+      return (
+        <p key={index}>{isLanguagePortuguese ? paragraph : enInformation._greetingMessage[index]}</p>
+      );
+    })
+  }
+
+  const renderProfilePicture = (): ReactNode => {
+    return (<img
+      src={ptInformation._profilePictureURL}
+      alt={isLanguagePortuguese ? ptInformation._profilePictureAltText : enInformation._profilePictureAltText}
+    />);
+  }
+
+  const renderLinksContainer = (): ReactNode => {
+    return (<div>
+      <Link.Root
+        ariaLabel={ariaLabel.links.github}
+        href={ptInformation._githubLink}
+        testid={dataTestIds.links.github}
+      >
+        <FaGithub />
+      </Link.Root>
+      <Link.Root
+        ariaLabel={ariaLabel.links.linkedin}
+        href={ptInformation._linkedinLink}
+        testid={dataTestIds.links.linkedin}
+      >
+        <FaLinkedin />
+      </Link.Root>
+    </div>);
+  }
+
+  const renderAboutMe = (): ReactNode => {
+    return (
       <article>
         {ptInformation._aboutMeText.map((paragraph, index) => (
           <p key={index}>{isLanguagePortuguese ? paragraph : enInformation._aboutMeText[index]}</p>
         ))}
       </article>
+    )
+  }
+
+  return (
+    <>
+      {renderHeader()}
+      <main>
+        {renderGreetingMessage()}
+        {renderProfilePicture()}
+      </main>
+      {renderLinksContainer()}
+      {renderAboutMe()}
     </>
   )
 }
