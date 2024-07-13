c1c3227f715feb87b193e0646aabca110f599e4b
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-07-13 17:05:49 -0300

    chore: Add pageUp and pageDown aria-label

--- src/helpers/ariaLabel.ts
+++ src/helpers/ariaLabel.ts
@@ -2,7 +2,9 @@ export const ariaLabel = {
   links: {
     linkedin: "LinkedIn link",
     github: "GitHub link",
-    email: "Email to"
+    email: "Email to",
+    pageDown: "Page down",
+    pageUp: "Page up",
   },
   pages: {
     projects: "Go to page Projects"
