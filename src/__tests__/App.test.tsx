175927b003f5d9fe8edd5f3258addc34829faf49
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-07-13 18:09:17 -0300

    tests: Update tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,11 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 import Information from "../helpers/classes/Information";
-import { vi } from "vitest";
+import { BrowserRouter as Router } from "react-router-dom";
 
 describe("Home page", () => {
   beforeEach(() => {
-    render(<App />);
+    render(
+      <Router>
+        <App />
+      </Router>
+    );
   });
 
   const enInformation = new Information("en");
