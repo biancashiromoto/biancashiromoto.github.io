31a123d67cef7708d3317ed6ccd7c6b5ece1eca1
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-07-13 17:21:27 -0300

    feat: Implement conditional rendering depending on language

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link as ReactLink } from "react-router-dom";
 import { ariaLabel } from "../../helpers/ariaLabel";
 import { FiArrowLeftCircle } from "react-icons/fi";
+import Information from "../../helpers/classes/Information";
 
 interface ProjectsProps {
   isLanguagePortuguese: boolean;
@@ -10,6 +11,8 @@ interface ProjectsProps {
 
 export const Projects = ({ isLanguagePortuguese, screenWidth }: ProjectsProps) => {
   const [fadeIn, setFadeIn] = useState<boolean>(false);
+  const ptInformation = new Information("pt");
+  const enInformation = new Information("en");
 
   return (
     <div className={`mt-20 ${fadeIn ? "fade-in" : ""}`}>
@@ -19,7 +22,7 @@ export const Projects = ({ isLanguagePortuguese, screenWidth }: ProjectsProps) =
         >
         <FiArrowLeftCircle />
       </ReactLink>
-      <h2 className="mb-5">In progress...</h2>
+      <h2 className="mb-5">{isLanguagePortuguese ? ptInformation._inProgress : enInformation._inProgress }</h2>
       <img 
         alt="Gif of a cat typing on a laptop"
         src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjRieWtkcXU0dmkzeGJhd2xueW1vOTg2N2Y5M2k0NHVoemRjMmxucSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/lJNoBCvQYp7nq/giphy.webp" />
