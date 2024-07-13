907bad71e2081ad4aa087f17afc4cf54e0754cf6
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-07-13 16:41:22 -0300

    chore: Add email aria-label

--- src/helpers/ariaLabel.ts
+++ src/helpers/ariaLabel.ts
@@ -1,7 +1,8 @@
 export const ariaLabel = {
   links: {
     linkedin: "LinkedIn link",
-    github: "GitHub link"
+    github: "GitHub link",
+    email: "Email to"
   },
   pages: {
     projects: "Go to page Projects"
