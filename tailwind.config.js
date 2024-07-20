07545e974ce1121dbe1b95aa1f08c6ff3ed11c09
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-07-20 11:54:52 -0300

    feat: Add osscilate animation to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,6 +72,8 @@ export const theme = {
     animation: {
       "accordion-down": "accordion-down 0.2s ease-out",
       "accordion-up": "accordion-up 0.2s ease-out",
+      "oscillate-down": "oscillate-up 2s infinite",
+      "oscillate-up": "oscillate-down 2s infinite",
     },
   },
 };
