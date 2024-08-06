5f0fa4db779b35c9e7ca045895a405116391e949
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-08-06 17:53:03 -0300

    refactor: Refactor data retrieve from GitHub

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -3,7 +3,7 @@ import { ariaLabel } from "../../helpers/ariaLabel";
 import { FiArrowLeftCircle } from "react-icons/fi";
 import Information from "../../helpers/classes/Information";
 import { Tooltip } from "../../components/Tooltip/Tooltip";
-import { memo, useEffect, useMemo, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import Utils from "../../helpers/classes/Utils";
 import {
   Carousel,
@@ -16,6 +16,7 @@ import {
   Card,
   CardContent,
 } from "../../../@shadcn-ui/components/ui/card"
+import axios from "axios";
 
 interface ProjectsProps {
   isLanguagePortuguese: boolean;
@@ -32,24 +33,36 @@ interface DataType {
 export const Projects = memo(({ isLanguagePortuguese }: ProjectsProps) => {
   const ptInformation = new Information("pt");
   const enInformation = new Information("en");
-  const { fetchData } = new Utils();
+  const { setLocalStorage, getLocalStorage } = new Utils();
   const [data, setData] = useState<DataType[]>([]);
 
-  useEffect( () => {
-    const fetchRepos = async () => {
-      const data = await fetchData();
-      setData(data);
-      console.log(data);
-      
+  useEffect(() => {
+    if (!getLocalStorage("repos")) {
+      const fetchRepos = async () => {
+        try {
+          const response = await axios.get(ptInformation._githubApiLink, {
+            headers: {
+              Authorization: ""
+            },
+          });
+          const filteredData = response.data.filter((project: DataType) => project.topics.includes("display"));
+          setLocalStorage("repos", filteredData);
+          setData(filteredData);
+        } catch (error) {
+          console.error('Error fetching repos:', error);
+        }
+      };
+      fetchRepos();
+    } else {
+      setData(getLocalStorage("repos"));
     }
-    fetchRepos();
-  }, []);
-
-  const filteredData = useMemo(() => data.filter((project) => project.topics.includes("display")), [data]);
+  }, [ptInformation._githubApiLink, getLocalStorage, setLocalStorage]);
 
   return (
-    <div className="page__projects  mt-14 relative">
+    <main className="page__projects">
+      {/*  TODO criar componente PageTitle */}
       <h1>{isLanguagePortuguese ? "Projetos" : "Projects"}</h1>
+      {/*  TODO criar componente LinkButton */}
       <Tooltip
         className="text-left text-3xl"
         text={isLanguagePortuguese ? ptInformation._returnToPreviousPageTooltip : enInformation._returnToPreviousPageTooltip}
@@ -57,38 +70,41 @@ export const Projects = memo(({ isLanguagePortuguese }: ProjectsProps) => {
         <ReactLink
           aria-label={ariaLabel.pages.return}
           to="/"
-          >
+        >
           <FiArrowLeftCircle />
         </ReactLink>
       </Tooltip>
       <h2 className="mb-5">{isLanguagePortuguese ? ptInformation._inProgress : enInformation._inProgress }</h2>
       <Carousel
+        className="w-full max-w-xs"
         opts={{
-          align: "start",
+          align: "center",
           loop: true,
         }}
-        className="w-full max-w-sm"
       >
-        <CarouselContent className="-ml-1">
-          {filteredData.filter((project) => project.topics.includes("display")).map((project) => {
-            const screenshotUrl = `https://raw.githubusercontent.com/biancashiromoto/${project.name}/main/screenshots/screenshot-01.`;
-            return (
-              <CarouselItem key={project.id} className="pl-1 md:basis-1/2 lg:basis-1/3">
-                <div className="p-1">
-                  <Card>
-                    <CardContent className="flex flex-col aspect-square items-center justify-center p-6">
-                      <h4>{project.name}</h4>
-                      <img alt={`Project ${project.name}'s screenshot`} src={`${screenshotUrl}${"png" || "gif"}`} />
-                    </CardContent>
-                  </Card>
-                </div>
-              </CarouselItem>
-            )
-          })}
-          <CarouselPrevious />
-        </CarouselContent>
-        <CarouselNext />
-      </Carousel>
-    </div>
-  )
-})
+      <CarouselContent>
+        {data.map((project, index) => {
+          const screenshotUrl = `https://raw.githubusercontent.com/biancashiromoto/${project.name}/main/screenshots/screenshot-01.png`;
+          return (
+            <CarouselItem key={index}>
+              <main className="p-1">
+                <Card>
+                  <CardContent className="flex flex-col aspect-square items-center justify-center p-10">
+                    <span className="text-4xl font-semibold">{project.name}</span>
+                    <img
+                      alt={`Project ${project.name}'s screenshot`}
+                      src={screenshotUrl}
+                    />
+                  </CardContent>
+                </Card>
+              </main>
+            </CarouselItem>
+          )
+        })}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+    </main>
+  );
+});
