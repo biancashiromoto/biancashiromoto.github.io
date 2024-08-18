026f6d58da822a40ff027b45b031a759ee4efcec
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-08-18 17:46:09 -0300

    refactor: Implement new Link component

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,38 +1,32 @@
-import { Link as ReactLink } from "react-router-dom";
 import { ariaLabel } from "../../helpers/ariaLabel";
 import { FiArrowLeftCircle } from "react-icons/fi";
 import Information from "../../helpers/classes/Information";
-import { Tooltip } from "../../components/Tooltip/Tooltip";
 import { memo } from "react";
 import Carousel from "../../components/Carousel";
-
-interface ProjectsProps {
-  isLanguagePortuguese: boolean;
-  screenWidth: number;
-}
+import { ProjectsProps } from "./index.types";
+import { Link } from "../../components/Link";
 
 export const Projects = memo(({ isLanguagePortuguese }: ProjectsProps) => {
   const ptInformation = new Information("pt");
   const enInformation = new Information("en");
 
   return (
-    <main className="page__projects--container">
-      {/*  TODO criar componente PageTitle */}
-      <h1>{isLanguagePortuguese ? "Projetos" : "Projects"}</h1>
-      {/*  TODO criar componente LinkButton */}
-      <Tooltip
-        className=""
-        text={isLanguagePortuguese ? ptInformation._returnToPreviousPageTooltip : enInformation._returnToPreviousPageTooltip}
-      >
-        <ReactLink
-          aria-label={ariaLabel.pages.return}
-          to="/"
+    <div className="pages__projects">
+      <main>
+        {/*  TODO criar componente PageTitle */}
+        <h1>{isLanguagePortuguese ? ptInformation._projects : enInformation._projects}</h1>
+        <Link.Root
+          ariaLabel={ariaLabel.pages.return}
+          className="return"
+          link="/"
+          target="_self"
+          testid="return"
+          text={isLanguagePortuguese ? ptInformation._returnToPreviousPageTooltip : enInformation._returnToPreviousPageTooltip}
         >
           <FiArrowLeftCircle />
-        </ReactLink>
-      </Tooltip>
-      <h2 className="mb-5">{isLanguagePortuguese ? ptInformation._inProgress : enInformation._inProgress }</h2>
-      <Carousel />
-    </main>
+        </Link.Root>
+        <Carousel />
+      </main>
+    </div>
   );
 });
