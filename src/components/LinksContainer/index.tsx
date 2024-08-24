aa0aa240a149cb0ff99e964a27d66162eabf2450
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-08-24 14:17:08 -0300

    refactor: Implement state management with Zustand

--- src/components/LinksContainer/index.tsx
+++ src/components/LinksContainer/index.tsx
@@ -1,13 +1,16 @@
 import { ariaLabel } from "../../helpers/ariaLabel";
 import { Link } from "../Link";
 import { dataTestIds } from "../../helpers/dataTestIds";
-import { LinksContainerProps } from './index.types';
 import Information from '../../helpers/classes/Information';
 import { FaGithub, FaLinkedin, FaProjectDiagram } from "react-icons/fa";
+import { useCounterStore } from "../../state/store";
 
-export const LinksContainer = ({ isLanguagePortuguese }: LinksContainerProps) => {
+export const LinksContainer = () => {
   const ptInformation = new Information("pt");
   const enInformation = new Information("en");
+
+  const { isLanguagePortuguese } = useCounterStore();
+
   return (
     <div className="flex gap-10 text-3xl items-center justify-center flex-wrap">
       <Link.Root
