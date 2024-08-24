47a79f4b7f4e02e19c5c1288327065632b82a734
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-08-24 14:38:23 -0300

    feat: Update store with information

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,18 +1,25 @@
 import { create } from 'zustand';
 import Utils from '../helpers/classes/Utils';
+import Information from '../helpers/classes/Information';
 
 const utils = new Utils();
+const ptInformation = new Information("pt");
+const enInformation = new Information("en");
 
 interface StoreState {
-  isLanguagePortuguese: boolean;
-  toggleLanguage: () => void;
+  enInformation: Information;
   fadeIn: boolean;
+  isLanguagePortuguese: boolean;
+  ptInformation: Information;
   toggleFadeIn: () => void;
+  toggleLanguage: () => void;
   screenWidth: number;
   setScreenWidth: (width: number) => void;
 }
 
 export const useCounterStore = create<StoreState>((set) => ({
+  enInformation,
+  ptInformation,
   isLanguagePortuguese: utils.isLanguagePortuguese(),
   toggleLanguage: () => set((prevState) => ({ isLanguagePortuguese: !prevState.isLanguagePortuguese })),
   fadeIn: false,
