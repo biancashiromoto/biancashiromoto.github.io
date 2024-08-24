1f9932a7ec86bda575e21b675dc4604a221c2491
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-08-24 16:31:32 -0300

    style: Implement SASS to style LinksContainer component

--- src/components/LinksContainer/index.tsx
+++ src/components/LinksContainer/index.tsx
@@ -13,38 +13,38 @@ export const LinksContainer = () => {
   } = useCounterStore();
 
   return (
-    <div className="flex gap-10 text-3xl items-center justify-center flex-wrap">
+    <div className="links-container">
       <Link.Root
-          ariaLabel={ariaLabel.pages.projects}
-          className="hover:scale-125 icon__projects"
-          link="/projects"
-          target="_self"
-          testid={dataTestIds.links.projects}
-          text={isLanguagePortuguese ? ptInformation._checkMyProjects : enInformation._checkMyProjects}
-        >
-          <FaProjectDiagram />
-        </Link.Root>
-        <Link.Root
-          ariaLabel={ariaLabel.links.github}
-          className="hover:scale-125 icon__github"
-          link={ptInformation._githubLink}
-          testid={dataTestIds.links.github}
-          text={ptInformation._gitHubTooltip}
-        >
-          <FaGithub />
-        </Link.Root>
-        <Link.Root
-          ariaLabel={ariaLabel.links.linkedin}
-          className="hover:scale-125 icon__linkedin"
-          link={ptInformation._linkedinLink}
-          testid={dataTestIds.links.linkedin}
-          text={ptInformation._linkedinTooltip}
-        >
+        ariaLabel={ariaLabel.pages.projects}
+        className="hover:scale-125"
+        link="/projects"
+        target="_self"
+        testid={dataTestIds.links.projects}
+        text={isLanguagePortuguese ? ptInformation._checkMyProjects : enInformation._checkMyProjects}
+      >
+        <FaProjectDiagram />
+      </Link.Root>
+      <Link.Root
+        ariaLabel={ariaLabel.links.github}
+        className="hover:scale-125"
+        link={ptInformation._githubLink}
+        testid={dataTestIds.links.github}
+        text={ptInformation._gitHubTooltip}
+      >
+        <FaGithub />
+      </Link.Root>
+      <Link.Root
+        ariaLabel={ariaLabel.links.linkedin}
+        className="hover:scale-125"
+        link={ptInformation._linkedinLink}
+        testid={dataTestIds.links.linkedin}
+        text={ptInformation._linkedinTooltip}
+      >
         <FaLinkedin />
       </Link.Root>
       <Link.Root
         ariaLabel={ariaLabel.links.resume}
-        className="hover:scale-125 icon__linkedin"
+        className="hover:scale-125"
         link={ptInformation._resumeLink}
         testid={dataTestIds.links.downloadMyCV}
         text={ptInformation._downloadMyCV}
