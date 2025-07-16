e003419677ad79200ac6ceae8937f9f35bebbb68
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-16 20:16:07 -0300

    refactor: simplify ESLint configuration by removing unnecessary plugins and rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,8 @@
 /* eslint-env node */
 
 module.exports = {
-  env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+    "next/core-web-vitals",
+    "next/typescript"
+  ]
 }
