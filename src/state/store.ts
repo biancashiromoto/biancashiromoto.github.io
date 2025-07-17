97cfba84e0ac6294704d8491d70c23a236889b72
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-17 19:13:03 -0300

    refactor: remove unused language-related state and methods from store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,31 +1,11 @@
-import { create } from 'zustand';
-import Utils from '../helpers/classes/Utils';
-import Information from '../helpers/classes/Information';
-
-const utils = new Utils();
-const ptInformation = new Information("pt");
-const enInformation = new Information("en");
+import { create } from "zustand";
 
 interface StoreState {
-  enInformation: Information;
   fadeIn: boolean;
-  isLanguagePortuguese: boolean;
-  ptInformation: Information;
   toggleFadeIn: () => void;
-  toggleLanguage: () => void;
-  screenWidth: number;
-  setScreenWidth: (width: number) => void;
-  initializeLanguage: () => void;
 }
 
 export const useCounterStore = create<StoreState>((set) => ({
-  enInformation,
-  ptInformation,
-  isLanguagePortuguese: false,
-  toggleLanguage: () => set((prevState) => ({ isLanguagePortuguese: !prevState.isLanguagePortuguese })),
   fadeIn: false,
   toggleFadeIn: () => set((prevState) => ({ fadeIn: !prevState.fadeIn })),
-  screenWidth: typeof window !== 'undefined' ? window.innerWidth : 1024,
-  setScreenWidth: (width) => set(() => ({ screenWidth: width })),
-  initializeLanguage: () => set(() => ({ isLanguagePortuguese: utils.isLanguagePortuguese() })),
 }));
