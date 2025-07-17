4b784721a9ed2efbc775ee46f6679e6d2f9ea8c6
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-17 18:46:39 -0300

    fix: streamline greeting message formatting in Information class

--- src/helpers/classes/Information.ts
+++ src/helpers/classes/Information.ts
@@ -26,14 +26,12 @@ export default class Information {
     this._profilePictureAltText = language === "en" ? "I am a woman with dark brown eyes and hair and I am wearing a black and gray T-shirt." : "Sou uma mulher de cabelos e olhos escuros e estou usando uma camiseta cinza e preta.";
     this._greetingMessage = language === "en" ? [
       "Hello!",
-      "My name is ",
-      "Bianca",
-      " and this is my portfolio!"
+      "My name is Bianca",
+      "and this is my portfolio!"
     ] : [
       "Olá!",
-      "Meu nome é ",
-      "Bianca",
-      " e esse é meu portfolio!"
+      "Meu nome é Bianca",
+      "e esse é meu portfolio!"
     ];
     this._aboutMeText = language === "en" ? [
       "Developer coming from a career transition in veterinary medicine, I worked for seven years as an anesthetist and intensivist before venturing into the world of technology. ",
