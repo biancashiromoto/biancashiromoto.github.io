fa5177e5a3761a1fd9420182f4233244c6215d2f
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-17 18:48:12 -0300

    refactor: update ScrollButton to use LanguageContext and improve code formatting

--- src/components/ScrollButton/index.tsx
+++ src/components/ScrollButton/index.tsx
@@ -1,26 +1,44 @@
-'use client'
+"use client";
 
-import { ScrollButtonProps } from './index.types'
-import { getAriaLabel } from '../../helpers/acessibility';
-import { dataTestIds } from '../../helpers/dataTestIds';
-import { FaRegArrowAltCircleDown, FaRegArrowAltCircleUp } from 'react-icons/fa';
-import { Button } from '../Button';
-import Utils from '../../helpers/classes/Utils';
-import { useCounterStore } from '../../state/store';
+import { useLanguage } from "@/src/context/LanguageContext";
+import { FaRegArrowAltCircleDown, FaRegArrowAltCircleUp } from "react-icons/fa";
+import { getAriaLabel } from "../../helpers/acessibility";
+import Utils from "../../helpers/classes/Utils";
+import { dataTestIds } from "../../helpers/dataTestIds";
+import { Button } from "../Button";
+import { ScrollButtonProps } from "./index.types";
 
-export const ScrollButton = ({ className, direction, href }: ScrollButtonProps) => {
+export const ScrollButton = ({
+  className,
+  direction,
+  href,
+}: ScrollButtonProps) => {
   const { scrollTo, scrollToTop } = new Utils();
-  const { isLanguagePortuguese } = useCounterStore();
+  const { isLanguagePortuguese } = useLanguage();
 
-  return(
+  return (
     <Button.Root
-      ariaLabel={direction === "up" ? getAriaLabel(isLanguagePortuguese).button.scrollUp : getAriaLabel(isLanguagePortuguese).button.scrollDown}
+      ariaLabel={
+        direction === "up"
+          ? getAriaLabel(isLanguagePortuguese).button.scrollUp
+          : getAriaLabel(isLanguagePortuguese).button.scrollDown
+      }
       // TODO remover estilização com tailwind
-      className={`${className} button__scroll ${direction === "up" ? "top-[15%]" : "bottom-[20%]"}`}
-      testId={direction === "up" ? dataTestIds.buttons.scrollUp : dataTestIds.buttons.scrollDown}
-      onClick={() => direction === "up" ? scrollToTop() : scrollTo(href)}
+      className={`${className} button__scroll ${
+        direction === "up" ? "top-[15%]" : "bottom-[20%]"
+      }`}
+      testId={
+        direction === "up"
+          ? dataTestIds.buttons.scrollUp
+          : dataTestIds.buttons.scrollDown
+      }
+      onClick={() => (direction === "up" ? scrollToTop() : scrollTo(href))}
     >
-      {direction === "down" ? <FaRegArrowAltCircleDown /> : <FaRegArrowAltCircleUp />}
+      {direction === "down" ? (
+        <FaRegArrowAltCircleDown />
+      ) : (
+        <FaRegArrowAltCircleUp />
+      )}
     </Button.Root>
   );
-}
+};
