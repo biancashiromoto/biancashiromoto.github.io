e6cc87054778a5ab79c91153808a7998643a01f1
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-19 18:21:10 -0300

    feat: implement scroll effect for Hero component image

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -2,13 +2,19 @@ import { FC } from "react";
 import styles from "./hero.module.scss";
 import Image from "next/image";
 import imageUrl from "@/public/assets/image__laptop.jpg";
+import { useHeroScrollEffect } from "./hooks/useHeroScrollEffect";
 
 const Hero: FC = () => {
+	const { imageRef } = useHeroScrollEffect();
+
 	return (
 		<section
 			className={styles.hero}
 		>
-			<div className={styles.imageContainer}>
+			<div
+				ref={imageRef}
+				className={styles.imageContainer}
+			>
 				<Image
 					src={imageUrl}
 					alt="Laptop"
