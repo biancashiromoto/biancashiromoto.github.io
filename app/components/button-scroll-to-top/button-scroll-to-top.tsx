96f1f5821fef294c607e34fa0af3d3a19b00be86
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-19 20:28:35 -0300

    feat: enhance ButtonScrollToTop component to conditionally display based on scroll progress

--- app/components/button-scroll-to-top/button-scroll-to-top.tsx
+++ app/components/button-scroll-to-top/button-scroll-to-top.tsx
@@ -1,13 +1,21 @@
 import { useLanguage } from "@/app/context/LanguageProvider";
 import styles from "./button-scroll-to-top.module.scss";
 import { BsArrowUp } from "react-icons/bs";
+import { useEffect, useState } from "react";
+import { useScroll } from "@/app/context/ScrollProvider";
 
 const ButtonScrollToTop = () => {
 	const { information } = useLanguage();
+	const [shouldShowButtonScrollToTop, setShouldShowButtonScrollToTop] = useState(true);
+	const { scrollProgress } = useScroll();
+
+	useEffect(() => {
+		setShouldShowButtonScrollToTop(scrollProgress > 80);
+	}, [scrollProgress]);
 
 	const handleScrollToTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
 
-	return (
+	return shouldShowButtonScrollToTop && (
 		<button
 			type="button"
 			className={styles["scroll-to-top"]}
