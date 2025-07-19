59c4843b74530a32d25aacfdc84b6ab35fe95f50
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-19 17:04:24 -0300

    refactor: format code in CustomLink component for improved readability

--- app/components/link/link.tsx
+++ app/components/link/link.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import React from "react";
 import Link from "next/link";
 import { LinkProps } from "./link.types";
 
 export const CustomLink = ({ path, children, target = "_self" }: LinkProps) => {
-  return (
-    <Link href={path} aria-label={`Link to ${path}`} target={target}>
-      {children}
-    </Link>
-  );
+	return (
+		<Link href={path} aria-label={`Link to ${path}`} target={target}>
+			{children}
+		</Link>
+	);
 };
