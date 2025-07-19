ac45ff3d80bcf58f4fdc1eafdb7b5a011a0df28d
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-19 19:34:53 -0300

    refactor: remove inline GreetingMessage component and simplify image props in Intro component

--- app/components/intro/intro.tsx
+++ app/components/intro/intro.tsx
@@ -3,34 +3,26 @@ import styles from "./intro.module.scss";
 import { altText } from "@/app/helpers/acessibility";
 import Image from "next/image";
 import { LinksContainer } from "../links-container";
-import Information from "@/app/helpers/classes/Information";
 import Hero from "../hero/hero";
 import { useWindowResize } from "@/app/context/WindowResizeProvider";
-
-const GreetingMessage = (information: Information) => {
-	return (
-		<div className={styles["intro__greeting-message"]}>
-			{information._greetingMessage.map((line, index) => (
-				<p key={index}>{line}</p>
-			))}
-		</div>
-	);
-};
+import GreetingMessage from "./greeting-message/greeting-message";
 
 const Intro = () => {
 	const { information, isLanguagePortuguese } = useLanguage();
 	const { width } = useWindowResize();
-	const isMobileOrTablet = width <= 1024;
+	const isMobileOrTablet = width <= 768;
+
+	const imageProps = {
+		alt: altText(isLanguagePortuguese).home.profilePicture,
+		src: information._profilePictureURL,
+		width: 200,
+		height: 200,
+		className: styles["profile-picture"],
+	};
 
 	return (
 		<div className={styles.intro}>
-			<Image
-				alt={altText(isLanguagePortuguese).home.profilePicture}
-				src={information._profilePictureURL}
-				width={200}
-				height={200}
-				className={styles["profile-picture"]}
-			/>
+			<Image {...imageProps} />
 			<GreetingMessage {...information} />
 			<LinksContainer />
 			{isMobileOrTablet && <Hero />}
