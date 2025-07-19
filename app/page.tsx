9b9eebba3ed8a382b3be5eef43bbcdb3b409b46e
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-19 14:59:10 -0300

    feat: simplify HomePage component by directly rendering AboutMe and removing unused language context

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,14 @@
 "use client";
 
 import styles from "@/app/styles/pages/home.module.scss";
-import { useLanguage } from "@/src/context/LanguageContext";
+import AboutMe from "./components/about-me/about-me";
 import Intro from "./components/intro/intro";
 
 function HomePage() {
-  const { information } = useLanguage();
-
-  const renderAboutMe = () => {
-    return (
-      <div>
-        {information._aboutMeText.map((_paragraph, index) => (
-          <p key={index}>{information._aboutMeText[index]}</p>
-        ))}
-      </div>
-    );
-  };
-
   return (
     <div className={styles.home} id="home-start">
       <Intro />
-      <article className="pages__home--about-me" id="about-me__container">
-        {renderAboutMe()}
-      </article>
+      <AboutMe />
     </div>
   );
 }
