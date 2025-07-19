3bf21a17cf7378f69ed30f0cbfd85231a3d67b05
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-19 17:15:47 -0300

    refactor: improve code formatting and alignment in LinksContainer component

--- app/components/links-container/index.tsx
+++ app/components/links-container/index.tsx
@@ -7,28 +7,22 @@ import { GrDocumentPdf } from "react-icons/gr";
 import styles from "./links-container.module.scss";
 
 export const LinksContainer = () => {
-  const { information } = useLanguage();
+	const { information } = useLanguage();
 
-  return (
-    <div className={styles["links-container"]}>
-      <Link
-        path={information._githubLink}
-        aria-label={information._gitHubTooltip}
-      >
-        <FaGithub />
-      </Link>
-      <Link
-        path={information._linkedinLink}
-        aria-label={information._linkedinTooltip}
-      >
-        <FaLinkedin />
-      </Link>
-      <Link
-        path={information._resumeLink}
-        aria-label={information._downloadMyCV}
-      >
-        <GrDocumentPdf />
-      </Link>
-    </div>
-  );
+	return (
+		<div className={styles["links-container"]}>
+			<Link path={information._githubLink} aria-label={information._gitHubTooltip}>
+				<FaGithub title={information._gitHubTooltip} />
+			</Link>
+			<Link
+				path={information._linkedinLink}
+				aria-label={information._linkedinTooltip}
+			>
+				<FaLinkedin title={information._linkedinTooltip} />
+			</Link>
+			<Link path={information._resumeLink} aria-label={information._downloadMyCV}>
+				<GrDocumentPdf title={information._downloadMyCV} />
+			</Link>
+		</div>
+	);
 };
