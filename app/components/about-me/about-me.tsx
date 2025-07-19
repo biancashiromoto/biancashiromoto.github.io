a14246a933a6603ebcf502b363d1ff88280654eb
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-19 20:37:28 -0300

    feat: enhance AboutMe component with proximity-based text color and character wrapping

--- app/components/about-me/about-me.tsx
+++ app/components/about-me/about-me.tsx
@@ -1,13 +1,20 @@
 import { useLanguage } from "@/app/context/LanguageProvider";
 import styles from "./about-me.module.scss";
+import { useProximityTextColor } from "./hooks/useProximityTextColor";
 
 const AboutMe = () => {
 	const { information } = useLanguage();
+	const proximityRef = useProximityTextColor("gray", 25);
+
+	const wrapText = (text: string) =>
+		text.split("").map((char, i) => (
+			<span data-char key={i}>{char}</span>
+		));
 
 	return (
-		<div className={styles["about-me"]} id="about-me">
+		<div className={styles["about-me"]} id="about-me" ref={proximityRef}>
 			{information._aboutMeText.map((_paragraph, index) => (
-				<p key={index}>{information._aboutMeText[index]}</p>
+				<p key={index}>{wrapText(information._aboutMeText[index])}</p>
 			))}
 		</div>
 	);
