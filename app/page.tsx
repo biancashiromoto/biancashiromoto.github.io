2417beb286aa833f6e74278191c118afa8dd6ac2
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-19 20:28:27 -0300

    feat: add ButtonScrollToTop component to HomePage for improved navigation

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@
 import styles from "@/app/styles/pages/home.module.scss";
 import AboutMe from "./components/about-me/about-me";
 import Intro from "./components/intro/intro";
+import ButtonScrollToTop from "./components/button-scroll-to-top/button-scroll-to-top";
 
 function HomePage() {
 	return (
 		<div className={styles.home} id="home-start">
 			<Intro />
 			<AboutMe />
+			<ButtonScrollToTop />
 		</div>
 	);
 }
