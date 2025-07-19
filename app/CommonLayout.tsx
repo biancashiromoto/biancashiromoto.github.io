3738ffed03dd81a20017bf7c64198d29ba0215b9
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-19 13:16:39 -0300

    feat: add CommonLayout component with header and children rendering

---
+++ app/CommonLayout.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Header from "./components/header/header";
+
+type CommonLayoutProps = {
+  children: React.ReactNode;
+};
+
+const CommonLayout = ({ children }: CommonLayoutProps) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default CommonLayout;
