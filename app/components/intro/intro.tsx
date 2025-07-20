79615c4fd61000136d5103a1e0e88fbf7c64acd6
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-19 23:51:40 -0300

    feat: simplify intro component by removing unused window resize logic and adjusting styles

--- app/components/intro/intro.tsx
+++ app/components/intro/intro.tsx
@@ -4,13 +4,10 @@ import { altText } from "@/app/helpers/acessibility";
 import Image from "next/image";
 import { LinksContainer } from "../links-container";
 import Hero from "../hero/hero";
-import { useWindowResize } from "@/app/context/WindowResizeProvider";
 import GreetingMessage from "./greeting-message/greeting-message";
 
 const Intro = () => {
 	const { information, isLanguagePortuguese } = useLanguage();
-	const { width } = useWindowResize();
-	const isMobileOrTablet = width <= 768;
 
 	const imageProps = {
 		alt: altText(isLanguagePortuguese).home.profilePicture,
@@ -25,7 +22,7 @@ const Intro = () => {
 			<Image {...imageProps} />
 			<GreetingMessage {...information} />
 			<LinksContainer />
-			{isMobileOrTablet && <Hero />}
+			<Hero />
 		</div>
 	);
 };
