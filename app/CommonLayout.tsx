1aab930d2308c116cb5a8d927944c828029b542e
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-23 19:58:38 -0300

    fix: update CommonLayout to use useLanguage hook for loading state management

--- app/CommonLayout.tsx
+++ app/CommonLayout.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import Header from "./components/header/header";
+import { useLanguage } from "./context/LanguageProvider";
 
 type CommonLayoutProps = {
   children: React.ReactNode;
 };
 
 const CommonLayout = ({ children }: CommonLayoutProps) => {
+	const { isLoading } = useLanguage();
+
 	return (
-		<>
+		<div className={!isLoading ? "loaded" : ""}>
 			<Header />
 			<main>{children}</main>
-		</>
+		</div>
 	);
 };
 
