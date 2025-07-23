9735116f03e122263bce6c4858c8c1db05d95bf7
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-23 19:00:38 -0300

    feat: replace button with switch component for language toggle in header

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,18 +1,20 @@
 import { useLanguage } from "@/app/context/LanguageProvider";
 import ProgressBar from "../progress-bar/progress-bar";
 import styles from "./header.module.scss";
+import Switch from "../switch/switch";
 
 const Header = () => {
-	const { information, toggleLanguage } = useLanguage();
+	const {
+		information, toggleLanguage, isLanguagePortuguese
+	} = useLanguage();
+
 	return (
 		<header className={styles.header}>
-			<button
-				aria-label={information._translateButtonLabel}
-				onClick={toggleLanguage}
-				type="button"
-			>
-				{information._translateButtonLabel.toUpperCase()}
-			</button>
+			<Switch
+				checked={!isLanguagePortuguese}
+				onChange={toggleLanguage}
+				label={information._translateButtonLabel}
+			/>
 			<ProgressBar />
 		</header>
 	);
