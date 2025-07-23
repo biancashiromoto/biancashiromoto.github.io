153871c5201a5828559d5e54db3667688c0c13cd
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-23 19:58:17 -0300

    refactor: format imageProps for better readability in Intro component

--- app/components/intro/intro.tsx
+++ app/components/intro/intro.tsx
@@ -10,9 +10,12 @@ const Intro = () => {
 	const { information, isLanguagePortuguese } = useLanguage();
 
 	const imageProps = {
-		alt: altText(isLanguagePortuguese).home.profilePicture, src: information._profilePictureURL,width: 200,
+		alt: altText(isLanguagePortuguese).home.profilePicture,
+		src: information._profilePictureURL,
+		width: 200,
 		height: 200,
 		className: styles["profile-picture"],
+		priority: true,
 	};
 
 	return (
