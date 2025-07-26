e062a8d7b7a6541444d6162b0bf2d85e0d576a97
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-26 18:54:20 -0300

    fix: restructure HomePage layout and adjust margin for better responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,20 @@ import styles from "@/app/styles/pages/home.module.scss";
 import AboutMe from "./components/about-me/about-me";
 import Intro from "./components/intro/intro";
 import ButtonScrollToTop from "./components/button-scroll-to-top/button-scroll-to-top";
+import Timeline from "./components/timeline/timeline";
+import { useWindowResize } from "./context/WindowResizeProvider";
+import Hero from "./components/hero/hero";
 
 function HomePage() {
+	const { width } = useWindowResize();
 	return (
 		<div className={styles.home} id="home-start">
-			<Intro />
-			<AboutMe />
+			<div className={styles.home__container}>
+				<Intro />
+				<AboutMe />
+			</div>
+			{width > 1023 && <Hero />}
+			<Timeline />
 			<ButtonScrollToTop />
 		</div>
 	);
