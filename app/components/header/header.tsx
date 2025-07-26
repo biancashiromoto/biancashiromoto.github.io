a2a9b8574aa64ba6dce8803de5ac19e0263f971d
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-26 15:01:37 -0300

    fix: replace switch component with language switch for improved clarity

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,7 +1,7 @@
 import { useLanguage } from "@/app/context/LanguageProvider";
 import ProgressBar from "../progress-bar/progress-bar";
 import styles from "./header.module.scss";
-import Switch from "../switch/switch";
+import LanguageSwitch from "../language-switch/language-switch";
 
 const Header = () => {
 	const {
@@ -10,7 +10,7 @@ const Header = () => {
 
 	return (
 		<header className={styles.header}>
-			<Switch
+			<LanguageSwitch
 				checked={!isLanguagePortuguese}
 				onChange={toggleLanguage}
 				label={information._translateButtonLabel}
