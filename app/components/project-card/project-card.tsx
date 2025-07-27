7c5e8e57556dcc46a17ba0d3279632f6643813a0
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-26 21:39:01 -0300

    feat: add ProjectCard component with repository details and read more functionality

---
+++ app/components/project-card/project-card.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Repository } from "@/app/helpers/classes/fetchRepos";
+import Image from "next/image";
+import Link from "next/link";
+import { useMemo } from "react";
+import { useReadMore } from "./hooks/useReadMore";
+import styles from "./project-card.module.scss";
+
+const ProjectCard = ({ repo }: { repo: Repository }) => {
+	const screenshotUrl = useMemo(() =>
+		`https://raw.githubusercontent.com/biancashiromoto/${repo.name}/main/screenshots/screenshot-01.png`, [repo.name]);
+
+	const {
+		displayText,
+		isExpanded,
+		shouldTruncate,
+		toggleReadMore
+	} = useReadMore(repo.description || "", 75);
+
+	return (
+		<div key={repo.id} className={styles["project-card"]}>
+			<h2>{repo.name}</h2>
+			<div className={styles.description}>
+				<p>{displayText}{shouldTruncate && !isExpanded && "..."}</p>
+				{shouldTruncate && (
+					<button
+						onClick={toggleReadMore}
+						className={styles.readMoreBtn}
+						type="button"
+					>
+						{isExpanded ? "Read less" : "Read more"}
+					</button>
+				)}
+			</div>
+			{repo.topics.length > 0 && (
+				<ul>
+					{repo.topics.filter(topic => topic !== "display").map(topic => (
+						<li key={topic}>{topic}</li>
+					))}
+				</ul>
+			)}
+			<div className={styles.links}>
+				<Link href={repo.html_url} target="_blank" rel="noopener noreferrer">View on GitHub</Link>
+				<span className={styles.dot}>•</span>
+				{repo.homepage && <Link href={repo.homepage} target="_blank" rel="noopener noreferrer">Deploy</Link>}
+			</div>
+			{screenshotUrl && (
+				<Image
+					src={screenshotUrl}
+					alt={`Screenshot of ${repo.name}`}
+					width={200}
+					height={300}
+				/>
+			)}
+		</div>
+	);
+};
+
+export default ProjectCard;
