b58ca090dd6914a9036b9bd42b03ca3df00957e1
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-26 22:32:20 -0300

    refactor: remove unused description and topics list logic from ProjectCard component

--- app/components/project-card/project-card.tsx
+++ app/components/project-card/project-card.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { Repository } from "@/app/helpers/classes/fetchRepos";
+import Utils from "@/app/helpers/classes/Utils";
 import Image from "next/image";
 import Link from "next/link";
 import { useMemo } from "react";
-import { useReadMore } from "./hooks/useReadMore";
 import styles from "./project-card.module.scss";
-import Utils from "@/app/helpers/classes/Utils";
+import ProjectDescription from "./project-description/project-description";
+import TopicsList from "./topics-list/topics-list";
 
 const utils = new Utils();
 
@@ -14,40 +15,16 @@ const ProjectCard = ({ repo }: { repo: Repository }) => {
 	const screenshotUrl = useMemo(() =>
 		`https://raw.githubusercontent.com/biancashiromoto/${repo.name}/main/screenshots/screenshot-01.png`, [repo.name]);
 
-	const {
-		displayText,
-		isExpanded,
-		shouldTruncate,
-		toggleReadMore
-	} = useReadMore(repo.description || "", 75);
-
 	return (
 		<div key={repo.id} className={styles["project-card"]}>
 			<h2>{utils.formatProjectTitle(repo.name)}</h2>
 			<div className={styles.content}>
-				<div className={styles.description}>
-					<p>{displayText}{shouldTruncate && !isExpanded && "..."}</p>
-					{shouldTruncate && (
-						<button
-							onClick={toggleReadMore}
-							className={styles.readMoreBtn}
-							type="button"
-						>
-							{isExpanded ? "Read less" : "Read more"}
-						</button>
-					)}
-				</div>
-				{repo.topics.length > 0 && (
-					<ul>
-						{repo.topics.filter(topic => topic !== "display").map(topic => (
-							<li key={topic}>{topic}</li>
-						))}
-					</ul>
-				)}
+				<ProjectDescription description={repo.description} />
+				<TopicsList topics={repo.topics} />
 				<div className={styles.links}>
 					<Link href={repo.html_url} target="_blank" rel="noopener noreferrer">View on GitHub</Link>
 					<span className={styles.dot}>•</span>
-					{repo.homepage && <Link href={repo.homepage} target="_blank" rel="noopener noreferrer">Deploy</Link>}
+					{repo.homepage && <Link href={repo.homepage} target="_blank" rel="noopener noreferrer">View deploy</Link>}
 				</div>
 			</div>
 			{screenshotUrl && (
