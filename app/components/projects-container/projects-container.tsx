d5ea60c574c8629380563700ebcf991fcf80b2b6
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-29 20:25:25 -0300

    feat: integrate language support in ProjectsContainer for improved user experience

--- app/components/projects-container/projects-container.tsx
+++ app/components/projects-container/projects-container.tsx
@@ -7,14 +7,14 @@ import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
 import styles from "./projects-container.module.scss";
+import { useLanguage } from "@/app/context/LanguageProvider";
 
 const utils = new Utils();
 const storagedRepos = utils.getLocalStorage("repos");
 
-console.log("storagedRepos", !storagedRepos);
-
 const ProjectsContainer = () => {
 	const [currentCardIndex, setCurrentCardIndex] = useState(0);
+	const { isLanguagePortuguese } = useLanguage();
 
 	const {
 		data: repos,
@@ -25,14 +25,14 @@ const ProjectsContainer = () => {
 		queryKey: ["repos"],
 		queryFn: fetchRepos,
 		enabled: !storagedRepos,
-		// staleTime: 1000 * 60 * 60 * 24, // 24 hours
+		staleTime: 1000 * 60 * 60 * 24, // 24 hours
 		initialData: storagedRepos ?? null,
 	});
 
-	if (isLoading) return <div>Loading...</div>;
+	if (isLoading) return <div>{isLanguagePortuguese ? "Carregando..." : "Loading..."}</div>;
 
 	if (isError || !repos) {
-		return <div>Error loading projects {error?.message ? `: ${error.message}` : ""}</div>;
+		return <div>{isLanguagePortuguese ? "Erro ao carregar projetos" : "Error loading projects"} {error?.message ? `: ${error.message}` : ""}</div>;
 	}
 
 	const goToNext = () => {
@@ -53,31 +53,34 @@ const ProjectsContainer = () => {
 	}
 
 	return (
-		<div
-			className={styles["projects-container"]}
-			tabIndex={0}
-			onKeyDown={handleKeyDown}
-		>
-			<button
-				type="button"
-				className={styles["scroll-previous"]}
-				onClick={goToPrevious}
-				aria-label="Projeto anterior"
+		<>
+			<h1>{isLanguagePortuguese ? "Projetos" : "Projects"}</h1>
+			<div
+				className={styles["projects-container"]}
+				tabIndex={0}
+				onKeyDown={handleKeyDown}
 			>
-				<BsArrowLeft />
-			</button>
-			{repos.length > 0 && (
-				<ProjectCard repo={repos[currentCardIndex]} />
-			)}
-			<button
-				type="button"
-				className={styles["scroll-next"]}
-				onClick={goToNext}
-				aria-label="Próximo projeto"
-			>
-				<BsArrowRight />
-			</button>
-		</div>
+				<button
+					type="button"
+					className={styles["scroll-previous"]}
+					onClick={goToPrevious}
+					aria-label="Projeto anterior"
+				>
+					<BsArrowLeft />
+				</button>
+				{repos.length > 0 && (
+					<ProjectCard repo={repos[currentCardIndex]} />
+				)}
+				<button
+					type="button"
+					className={styles["scroll-next"]}
+					onClick={goToNext}
+					aria-label="Próximo projeto"
+				>
+					<BsArrowRight />
+				</button>
+			</div>
+		</>
 	);
 };
 
