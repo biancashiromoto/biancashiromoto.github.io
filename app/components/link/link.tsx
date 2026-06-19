a72f9c583dcdb3c5ae9823529f0b6d7ef031ee7a
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2026-06-19 18:47:15 -0300

    refactor: remove unnecessary client directive from CustomLink component

--- app/components/link/link.tsx
+++ app/components/link/link.tsx
@@ -1,15 +1,11 @@
-"use client";
-
 import React from "react";
 import Link from "next/link";
 import { LinkProps } from "./link.types";
 
-export const CustomLink = ({
-	path, children, target = "_self"
-}: LinkProps) => {
-	return (
-		<Link href={path} aria-label={`Link to ${path}`} target={target}>
-			{children}
-		</Link>
-	);
+export const CustomLink = ({ path, children, target = "_self" }: LinkProps) => {
+  return (
+    <Link href={path} aria-label={`Link to ${path}`} target={target}>
+      {children}
+    </Link>
+  );
 };
