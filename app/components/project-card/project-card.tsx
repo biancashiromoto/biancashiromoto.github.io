3f9974251cc264e95237f07e89da1bc115e92cf1
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2026-06-20 19:50:48 -0300

    feat: change image loading strategy to lazy for ProjectCard component

--- app/components/project-card/project-card.tsx
+++ app/components/project-card/project-card.tsx
@@ -43,7 +43,7 @@ const ProjectCard = ({ repo }: { repo: Repository }) => {
           alt={`Screenshot of ${repo.name}`}
           width={200}
           height={300}
-          priority={true}
+          loading="lazy"
         />
       )}
     </div>
